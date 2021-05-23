0e0886d964d8d397bc5b95dd6e5403fa78a34f76
aliyah-qt <aliyahclayton.f@gmail.com>
2021-05-23 13:05:26 -0700

    A commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.send("<h1>Hello world!</h1>");
 });
 
 app.listen(3000);
